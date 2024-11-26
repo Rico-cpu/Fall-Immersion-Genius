6c761213d28932c712f4a3c9e0442f7ee42d934d
Rico-cpu <rico.green@hgs.hiddengeniusproject.org>
2024-11-25 17:41:19 -0800

    script.js

--- CS/javascript/model-pop-up/script.js
+++ CS/javascript/model-pop-up/script.js
@@ -1,3 +1,29 @@
-function modelPopUp() {
-    // Your code here.
-}   
+// Get modal element
+const modal = document.getElementById("myModal");
+
+//Get buttons
+const openModalBtn = document.getElementById("openModalBtn");
+const closeModalBtn = document.getElementById("closeModalBtn");
+const alertBtn = document.getElementById("alertBtn");
+
+//Open modal on button click
+openModalBtn.onclick = function() {
+    modal.style.display = "block";
+}
+
+//Close modal on close button click
+closeModalBtn.onclick = function() {
+    modal.style.display = "none";
+}
+
+//Close modal on outside click
+window.onclick = function(event) {
+    if (event.target === modal) {
+        modal.style.display = "none";
+    }
+}
+
+//Show alerton button click
+alertBtn.onclick = function() {
+    alert("This is a simple pop-up alert! Think about how much you learned today!!! Go Hidden Genius Project!");
+}
